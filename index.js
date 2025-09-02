8463b9bbc8fcb62820c80c984543b890d49002cf
HamidAliSE <hamidalig942@gmail.com>
2025-09-02 17:44:23 +0500

    add: useRenderCount hook
    Introduces a new useRenderCount hook to track component render counts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import useIsFirstRender from './src/useIsFirstRender';
 import useLifecycleLogger from './src/useLifecycleLogger';
 import useToggle from './src/useToggle';
+import useRenderCount from './src/useRenderCount';
 
 export default useIsFirstRender;
 export default useLifecycleLogger;
 export default useToggle;
+export default useRenderCount;
---
+++ src/useRenderCount.ts
@@ -0,0 +1,11 @@
+import { useRef } from 'react';
+
+const useRenderCount = () => {
+    const count = useRef(0);
+
+    count.current += 1;
+
+    return count.current;
+};
+
+export default useRenderCount;
