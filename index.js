48cec7c788c36106207628fecef5544305a7a645
HamidAliSE <hamidalig942@gmail.com>
2025-09-03 10:42:04 +0500

    add: useAppState hook
    Introduces a new useAppState hook for tracking the React Native AppState

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import useIsFirstRender from './src/useIsFirstRender';
 import useLifecycleLogger from './src/useLifecycleLogger';
 import useToggle from './src/useToggle';
 import useRenderCount from './src/useRenderCount';
+import useAppState from './src/useAppState';
 
-export default useIsFirstRender;
-export default useLifecycleLogger;
-export default useToggle;
-export default useRenderCount;
+export {
+    useIsFirstRender,
+    useLifecycleLogger,
+    useToggle,
+    useRenderCount,
+    useAppState,
+};
