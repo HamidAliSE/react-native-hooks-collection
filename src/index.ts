0930bdebdf02a5d9140d145ddf1d8d20ed529caa
HamidAliSE <hamidalig942@gmail.com>
2025-09-05 11:38:44 +0500

    config: typescript support

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import useIsFirstRender from './hooks/useIsFirstRender';
+import useLifecycleLogger from './hooks/useLifecycleLogger';
+import useToggle from './hooks/useToggle';
+import useRenderCount from './hooks/useRenderCount';
+import useAppState from './hooks/useAppState';
+
+export {
+    useIsFirstRender,
+    useLifecycleLogger,
+    useToggle,
+    useRenderCount,
+    useAppState,
+};
