678904dd7d7bbcce5b08ef098e0a3f26d299e539
HamidAliSE <hamidalig942@gmail.com>
2025-09-09 18:14:34 +0500

    refactor: central exports

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,2 @@
-import {
-    useIsFirstRender,
-    useLifecycleLogger,
-    useToggle,
-    useRenderCount,
-    useAppState,
-    useCountdown,
-    useCounter,
-} from './hooks/core';
-
-import {
-    useInternetStatus,
-} from './hooks/internet';
-
-export {
-    useIsFirstRender,
-    useLifecycleLogger,
-    useToggle,
-    useRenderCount,
-    useAppState,
-    useCountdown,
-    useCounter,
-    useInternetStatus,
-};
+export * from "./hooks/core";
+export * from "./hooks/internet";
